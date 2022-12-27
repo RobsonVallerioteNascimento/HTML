29757ed594b4ef50dcdc52e39944a1bd69b09ef1
Robson Valleriote Nascimento <93871640+RobsonVall@users.noreply.github.com>
2022-12-27 13:31:40 -0300

    Menu
    Menu DropDwon simples

---
+++ Menu-DropDown/scripts/script.js
@@ -0,0 +1,13 @@
+function mostraS1() {
+    document.getElementById("smenu1").style.visibility="visible";
+    document.getElementById("smenu2").style.visibility="hidden";
+}
+
+function mostraS2() {
+    document.getElementById("smenu1").style.visibility="hidden";
+    document.getElementById("smenu2").style.visibility="visible";
+}
+
+function escondeSub(obj) {
+    obj.style.visibility="hidden";
+}
